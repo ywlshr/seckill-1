91ff341094fb9c615b0e33ad81f745347ca45fdd
eagle <ywlshr@126.com>
2017-08-12 23:26:51 +0800

    three class meun

--- src/main/java/org/seckill/web/TestController.java
+++ src/main/java/org/seckill/web/TestController.java
@@ -23,15 +23,33 @@ public class TestController {
     @ResponseBody
     public Map exposer() {
         Map map = new HashMap();
-        List<String> list = new ArrayList<String>();
-        list.add("meun11");
-        list.add("meun12");
-        list.add("meun13");
-        list.add("meun14");
-        list.add("meun15");
-        map.put("meun1", list);
-        map.put("meun2", list);
-        map.put("meun3", list);
+        List<String> list1 = new ArrayList<String>();
+        list1.add("meun11");
+        list1.add("meun12");
+        list1.add("meun13");
+        list1.add("meun14");
+        list1.add("meun15");
+        map.put("meun1", list1);
+        List<String> list2 = new ArrayList<String>();
+        list2.add("meun21");
+        list2.add("meun22");
+        list2.add("meun23");
+        map.put("meun2", list2);
+        List<String> list3 = new ArrayList<String>();
+        list3.add("meun31");
+        list3.add("meun32");
+        list3.add("meun33");
+        map.put("meun3", list3);
+        List<String> list22 = new ArrayList<String>();
+        list22.add("meun221");
+        list22.add("meun222");
+        list22.add("meun223");
+        map.put("meun22", list22);
+//        List<String> list222 = new ArrayList<String>();
+//        list222.add("meun2221");
+//        list222.add("meun2222");
+//        list222.add("meun2223");
+//        map.put("meun222", list222);
         return map;
     }
 
--- src/main/webapp/resource/script/meun.js
+++ src/main/webapp/resource/script/meun.js
@@ -9,10 +9,22 @@ $(function(){
                 alert("sddd0");
                 for (var i in data)
                 {
-                    $('#mymeun').append("<li class=\"dropdown-submenu\"><a href=\"#\" id=\'" + i +"\'>" + i + "</a><ul class=\"dropdown-menu\"></a>");
+                    $('#mymeun').append("<li class=\"dropdown-submenu\"><a href=\"#\" id=\'" + i +"\'>" + i + "</a><ul class=\"dropdown-menu\"></a></ul></li>");
                     for(var j in data[i])
                     {
-                        $('#' + i).next(':first').append("<li><a href=\"#\" id=\'" + data[i][j] +"\'>" + data[i][j] + "</a></li>");
+                        var k = data[i][j];
+                        $('#' + i).next(':first').append("<li class=\"dropdown-submenu\"><a href=\"#\" id=\'" + k +"\'>" + k + "</a><ul class=\"dropdown-menu\"></a></ul></li>");
+                        if (data[k] != null)
+                        {
+                            console.log(k);
+                            console.log(data[k]);
+                            for(var key in data[k])
+                            {
+                                console.log("key:" + key);
+                                console.log("value:" + data[k][key]);
+                                $('#' +k).next(':first').append("<li><a href=\"#\" id=\'" + data[k][key] +"\'>" + data[k][key] + "</a></li>");
+                            }
+                        }
                     }
                     $("#btn1").html(i);
                 }
